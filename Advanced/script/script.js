bdcea0f0795109dd907a4a8434cefda9d9efad1f
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-03-03 10:26:02 +0100

    Advanced done

---
+++ Advanced/script/script.js
@@ -0,0 +1,30 @@
+// Advanced Exercise 01
+
+var a = 7;
+var b = 14;
+var c = "21";
+var d = '36';
+var e = 42;
+
+let sum = a + b + parseInt(c) + parseInt(d) + e;
+console.log(sum);
+
+var f = '1';
+var g = 15;
+var h = 8;
+var i = "1";
+
+let product = parseInt(f) * g * h * parseInt(i);
+console.log(product);
+
+document.write(sum / product);
+
+// Advanced Exercise 02
+
+let multiArr = [ [1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 25, 7], [21, 4, 6, 17], [3, 5, 26, 3] ];
+
+console.log(multiArr[1][1]);
+console.log(multiArr[4][2]);
+console.log(multiArr[5][3]);
+console.log(multiArr[2][3]);
+console.log(multiArr[2][1]);
