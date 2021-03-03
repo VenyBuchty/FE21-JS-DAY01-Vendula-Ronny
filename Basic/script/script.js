0070168e828e462e52e18ef17d3a390880ca9482
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-03-03 10:16:48 +0100

    Basic done

---
+++ Basic/script/script.js
@@ -0,0 +1,9 @@
+// console.log("Hi my name is Martin")
+
+// Basic Exercise 03
+let age = 25;
+console.log(`Hi my name is Martin and I am ${age} years old`);
+
+// Basic Exercise 04
+let players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+console.log(players[2])
